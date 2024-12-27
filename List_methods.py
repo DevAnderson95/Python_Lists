67d8c9016b2c847ff0fc4acff995d450276ad272
DevAnderson95 <DevAnderson95@gmail.com>
2024-12-26 21:13:31 -0500

    first commit

---
+++ List_methods.py
@@ -0,0 +1,7 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and attended:
+    print("Alice both submitted an assignment and attended the class! ")
+else:
+    print("Looks like she only chose one of the two tasks.")
---
+++ list_transform.py
@@ -0,0 +1,12 @@
+# Task 1 
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+sorted_grades = sorted(grades, reverse=True)
+print(sorted_grades)
+
+# Task 2 
+
+grade_avg = sum(grades)/len(grades)
+print(grade_avg)
+
+
